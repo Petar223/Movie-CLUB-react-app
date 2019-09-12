2220c13bb2af3610c6f6812b7c1d1172d15cda47
Petar Stankovic <stankovicpetar23@gmail.com>
2019-09-12 16:59:17 +0200

    Modified

--- src/components/Content/Navigation.js
+++ src/components/Content/Navigation.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Navigation() {
-  return (
-    <nav className="sidebar">
-      <ul className="side-nav">
-        <li className="side-nav__item">
-          <button id="film" className="side-nav__button">
-            <i className="fas fa-film"></i>
-            <span>Movies</span>
-          </button>
-        </li>
-        <li className="side-nav__item">
-          <button id="tv" className="side-nav__button">
-            <i className="fas fa-tv"></i>
-            <span>TV Shows</span>
-          </button>
-        </li>
+class Navigation extends Component {
+  state = {
+    className: "side-nav__button",
+    film: "film",
+    tv: "tv",
+    favorites: "favorites"
+  };
+  changeClass = () => {
+    this.setState({
+      className: "side-nav__button-active"
+    });
+  };
+  render() {
+    return (
+      <nav className="sidebar">
+        <ul className="side-nav">
+          <li className="side-nav__item">
+            <button
+              id={this.state.film}
+              className={this.state.className}
+              onClick={this.changeClass}
+            >
+              <i className="fas fa-film"></i>
+              <span>Movies</span>
+            </button>
+          </li>
+          <li className="side-nav__item">
+            <button id={this.state.tv} className={this.state.className}>
+              <i className="fas fa-tv"></i>
+              <span>TV Shows</span>
+            </button>
+          </li>
 
-        <li className="side-nav__item">
-          <button id="favorites" className="side-nav__button">
-            <i className="fas fa-heart"></i>
-            <span>Favorites</span>
-          </button>
-        </li>
-      </ul>
-      <div className="legal">&copy; 2019 by Petruska.</div>
-    </nav>
-  );
+          <li className="side-nav__item">
+            <button id={this.state.favorites} className={this.state.className}>
+              <i className="fas fa-heart"></i>
+              <span>Favorites</span>
+            </button>
+          </li>
+        </ul>
+        <div className="legal">&copy; 2019 by Petruska.</div>
+      </nav>
+    );
+  }
 }
 
 export default Navigation;
